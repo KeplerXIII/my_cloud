d0c53ec30ce6d162863a61d0689cddcd34bf9901
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2024-02-08 17:10:15 +0300

    upd

--- my_cloud/my_cloud/urls.py
+++ my_cloud/my_cloud/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import UserCreateView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/register/', UserCreateView.as_view(), name='user-create'),
 ]

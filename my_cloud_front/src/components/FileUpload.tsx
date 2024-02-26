527f96c115e1ce4d58635a20daf30d397b969681
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2024-02-26 17:35:05 +0300

    upd

--- my_cloud_front/src/components/FileUpload.tsx
+++ my_cloud_front/src/components/FileUpload.tsx
@@ -4,6 +4,7 @@ import { FileViewerType } from '../models'
 
 export const FileUpload = ({ userID, setData }: FileViewerType) => {
   const [file, setFile] = useState<File | null>(null)
+  const [inputKey, setInputKey] = useState(0)
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -30,6 +31,8 @@ export const FileUpload = ({ userID, setData }: FileViewerType) => {
       if (response.ok) {
         alert('Файл успешно загружен')
         fetchFiles(userID, setData)
+        setFile(null)
+        setInputKey((prevKey) => prevKey + 1)
       } else {
         alert('Ошибка при загрузке файла')
       }
@@ -40,7 +43,12 @@ export const FileUpload = ({ userID, setData }: FileViewerType) => {
 
   return (
     <div className="fileInputBlock">
-      <input className="fileInput" type="file" onChange={handleFileChange} />
+      <input
+        key={inputKey}
+        className="fileInput"
+        type="file"
+        onChange={handleFileChange}
+      />
       <button onClick={handleUpload}>Загрузить файл</button>
     </div>
   )

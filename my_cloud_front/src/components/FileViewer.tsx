00371a75c93e5593c39d665fd4c998b0a818ca87
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2024-02-26 18:17:19 +0300

    upd

--- my_cloud_front/src/components/FileViewer.tsx
+++ my_cloud_front/src/components/FileViewer.tsx
@@ -19,6 +19,9 @@ export const FileViewer = ({
             <strong>Оригинальное имя:</strong> {file.original_name},{' '}
             <strong>Размер:</strong> {file.size},{' '}
             <strong>Дата загрузки:</strong> {file.upload_date}
+            <button className="downloadButton">&#8595;</button> {/* Зеленая стрелка вниз - скачать */}
+            <button className="deleteButton">&#10060;</button> {/* Красный крестик - удалить */}
+            <button className="shareButton">&#128279;</button> {/* Значок поделиться */}
           </li>
         ))}
       </ul>

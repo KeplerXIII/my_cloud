2a18dae1443d9af3fba59c6ef8fe995e6fafb0bd
Alexander Pushkin <104095563+KeplerXIII@users.noreply.github.com>
2024-02-29 12:28:08 +0300

    Update ShareButton.tsx

--- my_cloud_front/src/components/files/buttons/ShareButton.tsx
+++ my_cloud_front/src/components/files/buttons/ShareButton.tsx
@@ -16,9 +16,9 @@ export const FileShareButton = ({ fileID, userID, setData }: FileButtonType) => 
 
       if (response.ok) {
         const data = await response.json()
-        await navigator.clipboard.writeText(data.special_link);
+        // await navigator.clipboard.writeText(data.special_link);
         fetchFiles(userID, setData)
-        alert(`Одноразовая ссылка для скачивания скопирована в буфер: ${data.special_link}`)
+        alert(`Одноразовая ссылка для скачивания: ${data.special_link}`)
       } else {
         const data = await response.json()
         console.error('Ошибка генерации ссылки:', data.message)

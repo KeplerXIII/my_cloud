c1f9398622838eac6dba657088b47324ffcf57e3
Alexander Pushkin <104095563+KeplerXIII@users.noreply.github.com>
2024-02-29 10:07:12 +0300

    Update routing.py

--- my_cloud_back/filestorage/routing.py
+++ my_cloud_back/filestorage/routing.py
@@ -2,7 +2,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
 
-from my_cloud_back.filestorage.consumer import NotificationConsumer
+from filestorage.consumer import NotificationConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/notification/$', NotificationConsumer.as_asgi()),
